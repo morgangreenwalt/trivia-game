7e01a9cdc49244b1b4819e47bcf56880ad206ba1
morgan greenwalt <you@example.com>
2017-05-14 11:58:58 -0500

    last edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,63 +2,69 @@ $(document).ready(function(){
 //Variables
 var audio = new Audio("assets/images/space-jam.mp3");
 var timer = 30;
-var input = $("<input>");
 var intervalId;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
-var userGuess = [];
-var quiz = [{
- 		"question": "Question 1: Who did the Spurs defeat to win the 2014 NBA title?",
-      	"choices": ["Cleavland Cavaliers", "Golden State Warriers", "Miami Heat", "Boston Celtics"],
-     	"correct": "Miami Heat",
+var questions = [{
+ 		question: "Question 1: Who did the Spurs defeat to win the 2014 NBA title?",
+      	choices: ["Cleavland Cavaliers", "Golden State Warriers", "Miami Heat", "Boston Celtics"],
+     	answer: "Miami Heat",
+     	name: "Q1",
     	}, 
 
-      	{"question": "Question 2: What animal is the Spurs mascot?",
-		"choices": ["Dog", "Bear", "Wolf", "Coyote"],
-		"correct": "Coyote",
+      	{question: "Question 2: What animal is the Spurs mascot?",
+		choices: ["Dog", "Bear", "Wolf", "Coyote"],
+		answer: "Coyote",
+		name: "Q2",
     	},
 
-    	{"question": "Question 3: In what year was David Robinson inducted into the Hall of Fame?",
-		"choices": ["2009", "2008", "2007", "2006"],
-		"correct": "2009",
+    	{question: "Question 3: In what year was David Robinson inducted into the Hall of Fame?",
+		choices: ["2009", "2008", "2007", "2006"],
+		answer: "2009",
+		name: "Q3",
     	},
     	
-    	{"question": "Question 4: What was Tim Duncan's jersey number with the Spurs?",
-		"choices": ["21", "31", "11", "1"],
-		"correct": "21",
+    	{question: "Question 4: What was Tim Duncan's jersey number with the Spurs?",
+		choices: ["21", "31", "11", "1"],
+		answer: "21",
+		name: "Q4",
     	}, 
 
-    	{"question": "Question 5: Who was the Championship MVP for the Spurs during the 2003 season?",
-		"choices": ["Tim Duncan", "David Robinson", "Tony Parker", "Manu Ginobli"],
-		"correct": "Tim Duncan",
+    	{question: "Question 5: Who was the Championship MVP for the Spurs during the 2003 season?",
+		choices: ["Tim Duncan", "David Robinson", "Tony Parker", "Manu Ginobli"],
+		answer: "Tim Duncan",
+		name: "Q5",
     	}]
 
+var unanswered = questions.length;
+
 //Click to begin timer
-$(".spurs-logo").on("click", function (){
-	$("#intro-copy").hide();
-	run();
-	audio.play();
+function displayQuestions() {
+	$(".spurs-logo").on("click", function (){
+		$(".intro-copy").hide();
+		run();
+		audio.play();
 
-	//List quiz questions
-	for (var i = 0; i < quiz.length; i++) {
-		$(".quiz").addClass("questions").append("<ul>" + quiz[i].question + "</ul>");
-		$(".quiz").append("<li class='choices'>" + "<input type='radio' name='select' value='choice1'/>" + "&nbsp;" + quiz[i].choices[0] + "</li");
-		$(".quiz").append("<li class='choices'>" + "<input type='radio' name='select' value='choice2'/>" + "&nbsp;" + quiz[i].choices[1] + "</li");
-		$(".quiz").append("<li class='choices'>" + "<input type='radio' name='select' value='choice3'/>" + "&nbsp;" + quiz[i].choices[2] + "</li");
-		$(".quiz").append("<li class='choices'>" + "<input type='radio' name='select' value='choice4'/>" + "&nbsp;" + quiz[i].choices[3] + "</li");
+		//List quiz questions
+		for (var i = 0; i < questions.length; i++) {
+			$(".quiz").append("<ul class='questions'> "+ questions[i].question +" </ul>");
 
-	}
-	//Display submit button
-	$(".submit").append("<button type='submit' class='btn btn-info'>" + "Submit" + "</button>");
-});
-
+			for (var j = 0; j < questions[i].choices.length; j++) {	
+				$(".quiz").append("<li class='choices'> <input type='radio' name=' "+ questions[i].name +" ' value='"+ questions[i].choices[j] +"'> "+ questions[i].choices[j] +" </li>");
+			}	
+		}
+		
+		//Display submit button
+		$(".submit").append("<button type='submit' class='btn btn-info'>" + "Submit" + "</button>");
+	});	
+}
+//Display submit button
 $(".submit").on("click", function(){
 		stop();
-		//guess();
-		//checkAnswers();
-		$(".answer-display").addClass("answers").append("<h4>" + "Correct Answers: " + correctAnswers + "</h4>");
-		$(".answer-display").addClass("answers").append("<h4>" + "Incorrect Answers: " + incorrectAnswers + "</h4>");
+		checkAnswers();
 });
+
+displayQuestions();
 
 //Function for timer
 function run() {
@@ -68,47 +74,74 @@ function run() {
 //Function for timer cont..    
 function decreaseNum() {
 	timer--;
-	$(".spurs-logo").html("<h1>" + timer + "</h1>");
+	$(".intro").html("<i class='fa fa-clock-o fa-spin fa-3x fa-fw'></i><span class='sr-only'></span><h1> "+ timer +" </h1>");
 
 	//  Once number hits zero...
 	if (timer === 0) {
 		stop();
+		$(".intro").hide();
 	}
 }
 
 //Function to stop game
 function stop() {
 	clearInterval(intervalId);
-	$(".quiz").html("<h2 class='quiz-end'>" + "Time is Up!" + "</h2>");
-    $(".quiz").append("<h2 class='quiz-end'>" + "Here's how you did: " + "</h2>");
+	// $(".intro").hide();
+	checkAnswers();
+	$(".quiz").addClass("backboard").html("<h2 class='quiz-end'>Time is Up!" + "</h2>");
+    $(".answer-display").append("<h2>Here's how you did: " + "</h2>");
+    // $(".replay").append("<button class='btn btn-info'>" + "Replay" + "</button>");
+    $(".answer-display").addClass("answers").append("<h4>Correct Answers: " + correctAnswers + "</h4>");
+    $(".answer-display").addClass("answers").append("<h4>Incorrect Answers: " + incorrectAnswers + "</h4>");
+    $(".answer-display").addClass("answers").append("<h4>Unanswered: " + unanswered + "</h4>");
     audio.pause();
     $(".submit").hide();
-    $(".spurs-logo").hide(timer);
-    //checkAnswers();
+    $(".intro").hide(timer);
+    
 }
 
-// function guess() {
-// 	if (quiz.elements[i].type === 'radio') {
-//           if (form.elements[i].checked === true) {
-//                 radioResults += form.elements[i].value + ' ';
-//             }
-//         }
+function checkAnswers() {
+    var radios = $("input[type='radio']");
+    var answersSelected = radios.filter(":checked").map(function() {
+        var answerObject = {}
+        var answeredQuestion = $(this).attr("name");
+        var answerValue = $(this).val();
+        var questionIndex = answeredQuestion.slice(-1) - 1;
 
+        answerObject.questionAnswered = answeredQuestion;
+        answerObject.userAnswer = answerValue;
+        answerObject.questionIndex = questionIndex;
+
+        return answerObject
+    });
+
+    for (var i = 0; i < answersSelected.length; i++) {
+        var userGuess = answersSelected[i].userAnswer;
+        var masterIndex = answersSelected[i].questionIndex;
+        var correctAnswer = questions[0,1,2,3,4].answer;
+	        if (userGuess === correctAnswer) {
+	            correctAnswers++;
+
+	        } 
+
+	        else {
+	            incorrectAnswers++;
+	        }
+        //compute unanswered count 
+        unanswered = questions.length - answersSelected.length;
+    }
+}
+// replay();
+
+// function replay() {
+// 	$(".replay").on("click", function(){
+// 		displayQuestions();
+// 		run();
+// 		$(".spurs-logo").html("<h1> "+ timer +" </h1>");
+// 		audio.play();
+// 		timer = 30;
+// 		correctAnswers = 0;
+// 		incorrectAnswers = 0;
+// 	});
 // }
-
-//Function to check game answers
-// function checkAnswers(){
-// 	for (var i = 0; i < quiz.length; i++) {
-// 		if (quiz[i].choices.input[type=radio].checked === quiz[i].correct) {
-// 			correctAnswers++;
-// 			$(".answer-display").html("<h4 class='answers'>" + correctAnswers + "</h4>");
-// 		}
-
-// 		else {
-// 			incorrectAnswers++;
-// 			$(".answer-display").append("<div class='answers'>" + incorrectAnswers + "</div>");
-// 		}
-// 	}
-// }
-
 });    
